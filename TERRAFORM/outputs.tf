8fa7674b880a654962391733dc2c7c8c970ed584
oulai87 <oulaidonatien@gmail.com>
2023-03-13 15:35:46 -0500

    Infra Provisioning

---
+++ TERRAFORM/outputs.tf
@@ -0,0 +1,19 @@
+output "eip_public_ip" {
+  description = "Public IP of the EIP"
+  value       = aws_eip.nat_eip.public_ip
+}
+
+output "jenkins_public_ip" {
+  description = "Public IP of the Jenkins instance"
+  value       = aws_instance.Jenkins.public_ip
+}
+
+output "sonarqube_public_ip" {
+  description = "Public IP of the Sonarqube instance"
+  value       = aws_instance.Sonarqube.public_ip
+}
+
+output "ansible_public_ip" {
+  description = "Public IP of the Ansible instance"
+  value       = aws_instance.Ansible.public_ip
+}
